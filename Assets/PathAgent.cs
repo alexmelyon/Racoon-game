18b8de1b97167ed2cb9bac1ef52256f7b329c8b5
Alex Melyon <alexmelyon@gmail.com>
2019-11-16 18:36:56 +0500

    2d racoon

---
+++ Assets/PathAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathAgent : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 100F;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pathCreator.IsEmpty()) {
+            return;
+        }
+        Vector3 next = pathCreator.GetNext();
+        Vector3 pos = Vector3.MoveTowards(transform.position, next, speed * Time.deltaTime);
+        transform.position = pos;
+        if(Vector3.Magnitude(next - transform.position) < 1) {
+            pathCreator.RemoveNext();
+        }
+    }
+}
---
+++ Assets/PathCreator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour
+{
+    public GameObject pathPrefab;
+    class PathDot {
+        public Vector3 pos;
+        public GameObject go;
+    }
+    List<PathDot> pathList = new List<PathDot>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for (Touch touch : Touch in Input.touches) {
+        //     if (touch.phase == TouchPhase.Began) {
+        //         // Construct a ray from the current touch coordinates
+        //         var ray = Camera.main.ScreenPointToRay (touch.position);
+        //         if (Physics.Raycast (ray)) {
+        //             // Create a particle if hit
+        //             Instantiate (particle, transform.position, transform.rotation);
+        //         }
+        //     }
+        // }
+    
+        if(Input.GetMouseButton(0)) {
+            Vector3 mouse = Input.mousePosition;
+            Debug.Log("X " + mouse.x + " Y " + mouse.y);
+            RaycastHit hit;
+            bool r = Physics.Raycast(Camera.main.transform.position, mouse, out hit, 100F, 0);
+            if(r) {
+                
+            }
+            
+            PathDot dot = new PathDot();
+            dot.pos = mouse;
+            GameObject go = Instantiate(pathPrefab, mouse, Quaternion.identity);
+            // Destroy(go);
+            dot.go = go;
+            pathList.Add(dot);
+        }
+    }
+
+    public bool IsEmpty() {
+        return pathList.Count == 0;
+    }
+
+    public Vector3 GetNext() {
+        return pathList[0].pos;
+    }
+
+    public void RemoveNext() {
+        GameObject go = pathList[0].go;
+        // Debug.Log(go);
+        Destroy(go);
+        pathList.RemoveAt(0);
+    }
+}
