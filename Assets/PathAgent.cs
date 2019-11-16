0b3824facc7d2781a90cdce6419493558fa8b94e
Alex Melyon <alexmelyon@gmail.com>
2019-11-16 20:47:30 +0500

    3d

--- Assets/PathAgent.cs
+++ Assets/PathAgent.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class PathAgent : MonoBehaviour
 {
     public PathCreator pathCreator;
-    public float speed = 100F;
+    public float speed = 10F;
+    public GameObject floor;
 
+    void Start() {
+        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        bool warped = agent.Warp(floor.transform.position);
+        Debug.Log("START " + warped);
 
-    // Update is called once per frame
+        // StartCoroutine(HoldNavAgent(floor, GetComponent<NavMeshAgent>()));
+    }
+
+    // public IEnumerator HoldNavAgent(GameObject target, NavMeshAgent pathFinder)
+    // {
+    //     yield return new WaitForSeconds(0.1f);
+    //     pathFinder.enabled = true;
+    //     target = GameObject.FindGameObjectWithTag("NavTarget");
+    //     pathFinder.speed = speed;
+    //     pathFinder.SetDestination(target.transform.position);
+    // }
+
     void Update()
     {
         if(pathCreator.IsEmpty()) {
             return;
         }
         Vector3 next = pathCreator.GetNext();
-        Vector3 pos = Vector3.MoveTowards(transform.position, next, speed * Time.deltaTime);
-        transform.position = pos;
-        if(Vector3.Magnitude(next - transform.position) < 1) {
+        // Vector3 pos = Vector3.MoveTowards(transform.position, next, speed * Time.deltaTime);
+        // transform.position = pos;
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(next);
+        
+        
+        float magnitude = Vector3.Magnitude(next - transform.position);
+        // Debug.Log("DESTINATION " + magnitude);
+        if(magnitude < 1.5) {
             pathCreator.RemoveNext();
         }
     }
--- Assets/PathCreator.cs
+++ Assets/PathCreator.cs
@@ -5,56 +5,81 @@ using UnityEngine;
 public class PathCreator : MonoBehaviour
 {
     public GameObject pathPrefab;
-    class PathDot {
+    class PathDot
+    {
         public Vector3 pos;
         public GameObject go;
     }
     List<PathDot> pathList = new List<PathDot>();
+    private GameObject lastCreated;
 
     // Update is called once per frame
     void Update()
     {
-        // for (Touch touch : Touch in Input.touches) {
-        //     if (touch.phase == TouchPhase.Began) {
-        //         // Construct a ray from the current touch coordinates
-        //         var ray = Camera.main.ScreenPointToRay (touch.position);
-        //         if (Physics.Raycast (ray)) {
-        //             // Create a particle if hit
-        //             Instantiate (particle, transform.position, transform.rotation);
-        //         }
-        //     }
-        // }
-    
-        if(Input.GetMouseButton(0)) {
-            Vector3 mouse = Input.mousePosition;
-            Debug.Log("X " + mouse.x + " Y " + mouse.y);
-            RaycastHit hit;
-            bool r = Physics.Raycast(Camera.main.transform.position, mouse, out hit, 100F, 0);
-            if(r) {
-                
+        foreach (var touch in Input.touches)
+        {
+            Debug.Log("TOUCH 1");
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("TOUCH 2");
+                handleTouch(touch.position);
             }
-            
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("MOUSE");
+            Vector3 mouse = Input.mousePosition;
+            handleTouch(mouse);
+            //     PathDot dot = new PathDot();
+            //     dot.pos = mouse;
+            //     GameObject go = Instantiate(pathPrefab, mouse, Quaternion.identity);
+            //     dot.go = go;
+            //     pathList.Add(dot);
+        }
+    }
+
+    void handleTouch(Vector3 screePos)
+    {
+        var ray = Camera.main.ScreenPointToRay(screePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 worldPos = hit.point;
+
+            float minDistance = 1;
+            GameObject go = null;
+            if (lastCreated == null || lastCreated != null && Vector3.Magnitude(lastCreated.transform.position - worldPos) > minDistance)
+            {
+                if(lastCreated != null) {
+                    Debug.Log("LAST MAGN " + Vector3.Magnitude(lastCreated.transform.position - worldPos));
+                }
+                go = Instantiate(pathPrefab, worldPos, Quaternion.identity);
+                lastCreated = go;
+            }
             PathDot dot = new PathDot();
-            dot.pos = mouse;
-            GameObject go = Instantiate(pathPrefab, mouse, Quaternion.identity);
-            // Destroy(go);
             dot.go = go;
+            dot.pos = worldPos;
             pathList.Add(dot);
         }
     }
 
-    public bool IsEmpty() {
+    public bool IsEmpty()
+    {
         return pathList.Count == 0;
     }
 
-    public Vector3 GetNext() {
+    public Vector3 GetNext()
+    {
         return pathList[0].pos;
     }
 
-    public void RemoveNext() {
+    public void RemoveNext()
+    {
         GameObject go = pathList[0].go;
-        // Debug.Log(go);
-        Destroy(go);
+        if(go != null) {
+            Destroy(go);
+        }
         pathList.RemoveAt(0);
     }
 }
