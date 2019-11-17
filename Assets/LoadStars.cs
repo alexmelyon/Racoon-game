f8f19ad79c7f22576bb202d1c0d64c521dbdd30b
RedOWL <vda.perm@mail.ru>
2019-11-17 21:35:42 +0500

    UPD PlayerPrefs

--- Assets/LoadStars.cs
+++ Assets/LoadStars.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class LoadStars : MonoBehaviour
 {
-    public GameObject level_preview_1;
-    public GameObject level_preview_2;
+    public GameObject[] level_preview;
 
     private int stars1, stars2;
 
     // Start is called before the first frame update
     void Start()
     {
-        stars1 = int.Parse(LoadConfigs("stars.cfg").Split(',')[0]);
-        stars2 = int.Parse(LoadConfigs("stars.cfg").Split(',')[1]);
+        //stars1 = int.Parse(LoadConfigs("stars.cfg").Split(',')[0]);
+        //stars2 = int.Parse(LoadConfigs("stars.cfg").Split(',')[1]);
 
-        for (int i = 3; i > stars1; i--)
-            level_preview_1.GetComponentsInChildren<Transform>()[1].gameObject.SetActive(false);
+        stars1 = PlayerPrefs.GetInt("Stars1", 0);
+        stars2 = PlayerPrefs.GetInt("Stars2", 0);
 
-        for (int i = 3; i > stars2; i--)
-            level_preview_2.GetComponentsInChildren<Transform>()[1].gameObject.SetActive(false);
+        foreach(GameObject preview in level_preview)
+            for (int i = 3; i > stars1; i--)
+                preview.GetComponentsInChildren<Transform>()[1].gameObject.SetActive(false);
 
         // Выборка по всем детям
         //foreach (Transform child in level_preview_2.GetComponentsInChildren<Transform>())
@@ -35,23 +35,23 @@ public class LoadStars : MonoBehaviour
         
     }
 
-    public string LoadConfigs(string file)
-    {
-        // Check File Exists
-        if (!File.Exists(Application.persistentDataPath+"/"+file))
-        { // No File
-            SaveConfigs("stars.cfg", "0,0");
-            return "0,0";
-        }
+    //public string LoadConfigs(string file)
+    //{
+    //    // Check File Exists
+    //    if (!File.Exists(Application.persistentDataPath+"/"+file))
+    //    { // No File
+    //        SaveConfigs("stars.cfg", "0,0");
+    //        return "0,0";
+    //    }
 
-        // Load File
-        string _data = File.ReadAllText(Application.persistentDataPath + "/" + file); // Read All Text
-        return _data;
-    }
+    //    // Load File
+    //    string _data = File.ReadAllText(Application.persistentDataPath + "/" + file); // Read All Text
+    //    return _data;
+    //}
 
-    public void SaveConfigs(string file, string data)
-    {
-        // Save File
-        File.WriteAllText(Application.persistentDataPath + "/" + file, data); // Save datas to file
-    }
+    //public void SaveConfigs(string file, string data)
+    //{
+    //    // Save File
+    //    File.WriteAllText(Application.persistentDataPath + "/" + file, data); // Save datas to file
+    //}
 }
