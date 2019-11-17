2b14cc57d8e966f2e097bdfa13e70d9ee0977193
Alex Melyon <alexmelyon@gmail.com>
2019-11-17 20:05:07 +0500

    Generate colliders

--- Assets/RayTrace.cs
+++ Assets/RayTrace.cs
@@ -32,19 +32,51 @@ public class RayTrace : MonoBehaviour
         RaycastHit NoseHit, TailHit;
         float maxDistance = 50F;
         if (Physics.Raycast(nose.transform.position, victim.transform.position - nose.transform.position, out NoseHit, maxDistance))
-            if (NoseHit.collider.gameObject == victim)
+        {
+            Debug.Log("NOSE " + NoseHit.collider.gameObject.name);
+            if(NoseHit.collider.gameObject.transform.IsChildOf(victim.transform))
+            {
                 if (Physics.Raycast(tail.transform.position, victim.transform.position - tail.transform.position, out TailHit, maxDistance))
-                    if (TailHit.collider.gameObject == victim)
+                {
+                    // if (TailHit.collider.gameObject == victim)
+                    if (TailHit.collider.gameObject.transform.IsChildOf(victim.transform))
+                    {
                         if (NoseHit.distance < (TailHit.distance - Vector3.Magnitude(nose.transform.position - tail.transform.position) / 4))
+                        {
                             IfVisible(); // Если видит
-                        else IfInvisible();
+                        }
+                        else
+                        {
+                            IfInvisible();
+                        }
+                    }
                     else IfVisible(); // Если видит только носом   
-                else IfInvisible();
-
+                }
+                else
+                {
+                    IfInvisible();
+                }
+            }
+        }
         if (IsHunting && state != HunterState.Hunting)
             FollowVictim();
         else if (!IsHunting && state != HunterState.Roaming)
             FollowPath();
+
+        // RaycastHit hit;
+        // Vector3 dir = victim.transform.position - transform.position;
+        // // Debug.Log("RAYCAST");
+        // if(Physics.Raycast(transform.position, dir, out hit)) {
+        //     Debug.Log("RAYCAST 2 " + hit.collider.gameObject.name);
+        //     if(hit.collider.gameObject.transform.IsChildOf(victim.transform)) {
+        //     // if(hit.collider.gameObject.name == victim.name) {
+        //         Debug.Log("RAYCAST 3");
+        //         Vector3 point = hit.point;
+        //         Vector3 dir2 = point - transform.position;
+        //         float res = Mathf.Atan2(dir2.y, dir2.x);
+        //         Debug.Log("RES " + res);
+        //     }
+        // }
     }
 
     void IfVisible()
