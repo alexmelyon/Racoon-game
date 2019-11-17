a5b0d3ee3685bcfcbc82579d7a83b25a8b6a0196
Alex Melyon <alexmelyon@gmail.com>
2019-11-17 21:19:03 +0500

    Fixed teleport

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -9,11 +9,21 @@ public class Teleport : MonoBehaviour
 
     public GameObject exit;
 
+    private NavMeshAgent agent;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PathAgent>() != null) {
             other.gameObject.transform.position = another.exit.transform.position;
-            other.gameObject.GetComponent<NavMeshAgent>().SetDestination(another.exit.transform.position);
+            agent = other.gameObject.GetComponent<NavMeshAgent>();
+            agent.SetDestination(another.exit.transform.position);
+            StartCoroutine(clearAgent());
         }
+    }
+
+    IEnumerator clearAgent() {
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForSeconds(0.1F);
+        agent.SetDestination(agent.destination);
     }
 }
