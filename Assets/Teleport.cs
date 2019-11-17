54bfc2c4243c927d3b18951d91374022dcc0a56a
Alex Melyon <alexmelyon@gmail.com>
2019-11-17 21:10:00 +0500

    Teleport

---
+++ Assets/Teleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Teleport : MonoBehaviour
+{
+    public Teleport another;
+
+    public GameObject exit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PathAgent>() != null) {
+            other.gameObject.transform.position = another.exit.transform.position;
+            other.gameObject.GetComponent<NavMeshAgent>().SetDestination(another.exit.transform.position);
+        }
+    }
+}
